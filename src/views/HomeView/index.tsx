16699b9a5d63648582202c14debf9223fa050bc6
Ming Lin <minglianglin50@gmail.com>
2021-07-19 23:56:55 -0400

    remove

--- src/views/HomeView/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function HomeView() {
-  return (
-    <div>
-      <h1>Ming Lin</h1>
-    </div>
-  );
-}
--- src/views/LoadingView/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function LoadingView(props: any) {
-  return <h1>loading</h1>;
-}
