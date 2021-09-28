914a48201b12ddb17036b61636a4722ed1acd75a
Ming Lin <minglianglin50@gmail.com>
2021-09-28 11:53:25 -0400

    update github repo routes

--- src/types/enums/Routes.ts
+++ src/types/enums/Routes.ts
@@ -13,8 +13,8 @@ export enum EExternalRoute {
 
 export enum EGithubRepoLink {
   Gedcom = 'https://github.com/ming1in/gedcom-parser',
-  Zillow = 'https://github.com/ming1in/Stevens-IT/tree/master/CS_559/projects/03_zillow',
+  Zillow = 'https://github.com/ming1in/Stevens-IT/blob/master/21S_CS559_FundamentalsAndApplicationsOfMachineLearning/projects/03_zillow/project_3.ipynb',
   JimApp = 'https://github.com/ming1in/jim-app',
   LiveGithub = 'https://github.com/WellSheet/live-github',
-  Plaf = 'https://github.com/ming1in/Stevens-IT/tree/master/CS_496/interpreter'
+  Plaf = 'https://github.com/ming1in/Stevens-IT/tree/master/20F_CS496_ProgrammingLanguages/interpreter'
 }
